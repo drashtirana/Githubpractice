2c5e8a098ddcbd2a1d5f39ba9a2c3dcf3fa92bb0
drashtirana <drashtirana@outlookemail.ca>
2023-12-31 20:16:52 -0500

    substraction

---
+++ src/main/java/Minusprog.java
@@ -0,0 +1,13 @@
+
+public class Minusprog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=10;
+		int sum =b-a;
+		System.out.println("Total==="+sum);
+
+	}
+
+}
